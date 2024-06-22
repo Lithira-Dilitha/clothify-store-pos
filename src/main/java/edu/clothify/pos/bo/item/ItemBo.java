c3046f7d42d16c321fbba2f89c2f66e9c2dd0a74
Lithira-Dilitha <lithiradilitha2022@gmail.com>
2024-06-22 19:07:05 +0530

    created ItemBo layer

--- src/main/java/edu/clothify/pos/bo/item/ItemBo.java
+++ src/main/java/edu/clothify/pos/bo/item/ItemBo.java
@@ -1,6 +1,15 @@
 package edu.clothify.pos.bo.item;
 
 import edu.clothify.pos.bo.SuperBo;
+import edu.clothify.pos.dto.Employee;
+import edu.clothify.pos.dto.Item;
+
+import java.util.List;
 
 public interface ItemBo extends SuperBo {
+    boolean saveItem(Item item);
+    List<Item> getAllItemByIsActiveTrue();
+    Item getItemById(String itemId);
+    boolean updateItem(String itemId ,Item item);
+    boolean deleteItem(String itemId);
 }
--- src/main/java/edu/clothify/pos/bo/item/impl/ItemBoImpl.java
+++ src/main/java/edu/clothify/pos/bo/item/impl/ItemBoImpl.java
@@ -1,6 +1,33 @@
 package edu.clothify.pos.bo.item.impl;
 
 import edu.clothify.pos.bo.item.ItemBo;
+import edu.clothify.pos.dto.Item;
+
+import java.util.List;
 
 public class ItemBoImpl implements ItemBo {
+    @Override
+    public boolean saveItem(Item item) {
+        return false;
+    }
+
+    @Override
+    public List<Item> getAllItemByIsActiveTrue() {
+        return List.of();
+    }
+
+    @Override
+    public Item getItemById(String itemId) {
+        return null;
+    }
+
+    @Override
+    public boolean updateItem(String itemId, Item item) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteItem(String itemId) {
+        return false;
+    }
 }
